40b5898107c08b543b4650121ea8ad3e049b565a
DamDev1 <samueldamilare622@gmail.com>
2025-03-29 02:59:05 -0700

    Update Header component to use Next.js Link and Button components

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { Button } from '../ui/button'
-import Logo from './logo'
+import React from "react";
+import { Button } from "../ui/button";
+import Logo from "./logo";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className='p-5 flex justify-between items-center shadow-sm'>
-        <Logo/>
-        <nav className='md:flex gap-10 list-none hidden '>
-            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-blue-600'>Home</li>
-            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-blue-600'>Search</li>
-            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-blue-600'>Contact</li>
-            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-blue-600'>Win a car</li>
-        </nav>
-        <button className='text-white bg-blue-600 py-2 px-4 rounded-md cursor-pointer'>Sign in</button>
+    <header className="p-5 flex justify-between items-center shadow-sm">
+      <Logo />
+      <nav className="md:flex gap-10 list-none hidden ">
+        <li className="font-medium hover:scale-105 transition-all cursor-pointer hover:text-blue-600">
+          Home
+        </li>
+        <li className="font-medium hover:scale-105 transition-all cursor-pointer hover:text-blue-600">
+          Search
+        </li>
+        <li className="font-medium hover:scale-105 transition-all cursor-pointer hover:text-blue-600">
+          Contact
+        </li>
+        <li className="font-medium hover:scale-105 transition-all cursor-pointer hover:text-blue-600">
+          Win a car
+        </li>
+      </nav>
+     <Link href={'/login'}><Button>Sign in</Button></Link>
     </header>
-  )
+  );
 }
