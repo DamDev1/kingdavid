679651ca33f550901d63b049b6a32ae1b3a07e6d
DamDev1 <samueldamilare622@gmail.com>
2025-03-29 15:24:25 -0700

    Refactor login page: simplify layout and import structure

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,7 @@
 "use client"
-import { LoginForm } from "@/components/login/loginForm"
 import Logo from "@/components/shared/logo"
-
-export default function LoginPage() {
+import { SignupForm } from "@/components/signup/signupForm"
+export default function SignupPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
@@ -11,13 +10,13 @@ export default function LoginPage() {
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
-            <LoginForm />
+            <SignupForm />
           </div>
         </div>
       </div>
       <div className="relative hidden bg-muted lg:block">
         <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTmxFIJ9YKA4-P-oqN_th0SwXway_MHQkizw&s"
+          src="https://thumbs.dreamstime.com/b/luxury-cars-mercedes-benz-c-e-class-parkingin-row-car-store-russia-saint-petersburg-october-luxury-cars-mercedes-benz-c-128640828.jpg"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
