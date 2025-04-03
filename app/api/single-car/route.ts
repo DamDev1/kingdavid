d09422d5056d29c5bd3fab29129c96ae1fc42ad9
DamDev1 <samueldamilare622@gmail.com>
2025-04-03 12:26:07 -0700

    Adding get single car details

---
+++ app/api/single-car/route.ts
@@ -0,0 +1,36 @@
+import connectMongo from "@/lib/mongodb";
+import CarListing from "@/modals/CarListing";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+
+    try {
+        await connectMongo()
+
+        const carId = req.nextUrl.searchParams.get("id");
+
+        if (!carId) {
+            return NextResponse.json({ message: "Car ID is required" }, { status: 400 });
+        }
+        const car = await CarListing.findById(carId);
+
+        if (!car) {
+            return NextResponse.json({ message: "Car not found" }, { status: 404 });
+        }
+
+        return NextResponse.json({
+            message: "Car Fetched Successful",
+            data: car
+        }, { status: 200 })
+    } catch (error) {
+        console.error("Login error:", error);
+
+        return NextResponse.json(
+            {
+                message: "Failed to fetch",
+                error: error instanceof Error ? error.message : "Unknown error",
+            },
+            { status: 400 } // Always return a 400 Bad Request for handled errors
+        );
+    }
+}
