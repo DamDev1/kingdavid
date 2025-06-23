9ecfff3bbffa886e70ca3631df0b56eb1eb6a2cc
DamDev1 <samueldamilare622@gmail.com>
2025-06-23 01:54:19 -0700

    updating

--- config/emailTransporter.ts
+++ config/emailTransporter.ts
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'tubetroveofficial@gmail.com',
-    pass: 'tvzpgxjjolaczgow', // store this in .env
+    pass: 'tvzpgxjjolaczgow',
   },
 });
 
